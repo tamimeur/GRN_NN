0d1f8f63faf55b8c65635e62235aabb39042e945
Tileli Amimeur <Leli@D-69-91-179-173.dhcp4.washington.edu>
2016-11-18 16:17:13 -0800

    cnn5 is a working CNN for the nbt sharon paper

--- mod_gram_cnn5.py
+++ mod_gram_cnn5.py
@@ -7,13 +7,18 @@ from matplotlib import pyplot as plt
 from keras.utils import np_utils
 import keras.callbacks as cb
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
-from keras.optimizers import RMSprop, SGD
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.optimizers import RMSprop, SGD, Adam
 from keras.datasets import mnist
 from keras.layers import Merge
 from keras.regularizers import l1, l2, activity_l2
+from keras.layers.convolutional import Convolution1D, MaxPooling1D
 import random
 import pandas as pd
+from sklearn.cross_validation import train_test_split
+from sklearn import preprocessing
+import scipy.stats as stats
+import seaborn as sea
 
 
 class LossHistory(cb.Callback):
@@ -25,7 +30,7 @@ class LossHistory(cb.Callback):
         self.losses.append(batch_loss)
 
 
-np.random.seed(1337)
+#np.random.seed(1337)
 X_train = []
 Y_train = []
 
@@ -58,129 +63,93 @@ def format_data(char_data, des):
 		Y_train.append([val])
 
 	return X_train, Y_train
+
+
 ############# LOAD DATA - NBT PAPER ########
 pdata = pd.read_excel('nbt_sharon_pp.xlsx',header=0,parse_cols="C,G")
 
 
-print "Is YO DATA BOI!!! ", pdata
-print "length of data ", pdata.size
+print "Is YO DATA BOI!!! \n", pdata
+#print "length of data ", pdata.size
+print pdata.columns
+df = pdata[np.isfinite(pdata[u' expression'])]
+
+# print "cleaned data \n", df
+# print "length of cleaned data ", df.size
+# print "length of sequence ", len(df[u'sequence'][1])
+
+############# Format NN inputs #############
+
+def oneHotEncoder(seq):
+	base_dict = {u'A':[1,0,0,0],u'C':[0,1,0,0],u'G':[0,0,1,0],u'T':[0,0,0,1]}
+	return np.array([base_dict[x] for x in seq])
+
+X_data = np.empty([len(df),150,4])
+indx = 0
+
+Y_data = np.array(df[[u' expression']])
+
+for seq in df[u'sequence']:
+	X_data[indx] = oneHotEncoder(seq)
+	indx += 1
+
+print "CNN input \n", X_data
+print "CNN output \n", Y_data
 
 
-############# GENERATE LANGUAGE ############
-
-#The Grammar
-Signals = ('s')
-T = ('p1','p2','p3','p4','p5','g0','g1','g2')
-NN_inputs = []
-print "Neural N in struct = ", NN_inputs
-
-#Productions
-P = [ ('Design',           ['R']),                  \
-#('Design',          ['R','C']),                     \
-#('Design',          ['C','C','R']),                 \
-('C',               ['Prom', 'Gene']),              \
-('R',               ['Prom', 'g0']),                \
-('Prom',            ['p1']),                        \
-('Prom',            ['p2']),                        \
-('Prom',            ['p3']),                        \
-('Prom',            ['p4']),                        \
-('Prom',            ['p5']),                        \
-('Gene',            ['g1']),                        \
-('Gene',            ['g2'])                         ]
-
-language = parse_input.getLanguage(P,T)
-
-print "Language = ", language
-print "Num Designs = ", len(language)
-print "Language = ", language
-print "Longest design ", max(language, key=len)
-
-############# GENERATE SINGLE CAS N.N. MODEL ############
-start_time = time.time()
-num_inputs = 2
-num_hidden = 2
-print 'num inputs = ', num_inputs
-print 'num hidden units = ', num_hidden
-print 'Compiling Model ... '
-submodel = Sequential()
-submodel.add(Dense(num_hidden, input_dim=num_inputs, W_regularizer=l1(0.1))) #, init='zero'))
-submodel.add(Activation('relu'))
-submodel.add(Dropout(0.3))
-submodel.add(Dense(1))
-submodel.add(Activation('sigmoid'))
-
-rms = RMSprop()
-sgd = SGD(lr=0.01)
-submodel.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-print 'Model compiled in {0} seconds'.format(time.time() - start_time)
-print submodel.get_config()
-
-############# RUN AGENT ############
-############# SINGLE-CAS FORMAT DATA ############
-
-X_train = []
-Y_train = []
-#cur_design = ['p3','g0']
-#language.remove(cur_design)
+########## RANDOM TEST/TRAIN SPLIT #########
+X_train, X_test, y_train, y_test = train_test_split(X_data, Y_data, test_size=0.15, random_state=42)
+norm_train = preprocessing.StandardScaler().fit_transform(y_train)
+print "len X train: \n", len(X_train)
+print "len Y train: \n", len(y_train)
+print "len X test:  \n", len(X_test)
+print "len Y test:  \n", len(y_test)
 
 
-testi = 0
-langsize = int(len(language))
-print "LANGSIZE = ", langsize
-while testi<langsize:
-	cur_design = language[testi]
-	cur_char_data = lab.get_data(cur_design, testi)
-	X_train, Y_train = format_data(cur_char_data, cur_design)
-	# cur_design = random.choice(language)
-	# language.remove(cur_design)
-	testi += 1
+############# TRAIN NN ############
+#start_time = time.time()
+model = Sequential()
+model.add(Convolution1D(nb_filter=30,filter_length=6,input_dim=4,input_length=150,border_mode="same", activation='relu'))
+model.add(Dropout(0.1))
+model.add(Convolution1D(nb_filter=40,filter_length=6,input_dim=4,input_length=150,border_mode="same", activation='relu'))
 
-print "\n\nX_train for full single cas language = ", X_train
-print "\nY_train for full single cas language = ", Y_train
+model.add(Flatten())
 
-c = list(zip(X_train, Y_train))
-random.shuffle(c)
-X_train, Y_train = zip(*c)
+model.add(Dense(40))
+model.add(Dropout(0.2))
+model.add(Activation('relu'))
 
-print "\n\nShuffled X_train = ", X_train
-print "\nShuffled Y_train = ", Y_train
+model.add(Dense(1))
+model.add(Activation('linear'))
 
-Y_train_max = max(Y_train)[0]
-Y_train_min = min(Y_train)[0]
-print "\n\n Ymax ", Y_train_max
-print "\n\n Ymin ", Y_train_min
-Y_train_norm = []
+#compile the model
+adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+#model.compile(loss='mean_squared_error', optimizer=adam)
+rms = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08)
+model.compile(loss='mean_squared_logarithmic_error', optimizer=rms)
+#print 'Model compiled in {0} seconds'.format(time.time() - start_time)
 
-for y in Y_train:
-	Y_train_norm.append((y - Y_train_min) / (Y_train_max - Y_train_min))
-
-X_train = np.array(X_train)
+#train the model
+model.fit(X_train, norm_train, batch_size=128, nb_epoch=6, verbose=1)
 
 
-print "\n\nX_train = ", X_train
-#print "\nY_train = ", np.array(Y_train)
-Y_train = np.array(Y_train_norm)
-print "\nNormalized Y_train = ", Y_train
+########### RESULTS ##############
 
-# print "TRAINING DATA X: ", X_train
-# print "Y: ", Y_train
-Fin_xtrain = X_train[:len(X_train)/2]
-Fin_xtest = X_train[len(X_train)/2:]
+norm_test = preprocessing.StandardScaler().fit_transform(y_test)
+predicted = model.predict(X_test) #.reshape(-1)
+print "NORMED TEST: ", len(norm_test)
+print "PREDICTED: ", len(predicted)
+slope, intercept, r_value, p_value, std_err = stats.linregress(norm_test.reshape(-1),predicted.reshape(-1))
+print r_value**2
 
-Fin_ytrain = Y_train[:len(Y_train)/2]
-Fin_ytest = Y_train[len(Y_train)/2:]
+d = {'y_pred': predicted.reshape(-1), 'y_actual': norm_test.reshape(-1)}
+res_df = pd.DataFrame(data=d)
 
-print "\n\nFin_xtrain n test = ", Fin_xtrain, Fin_xtest
-
-##################### TRAIN SINGLE CAS MODEL #######################
-submodel.fit(Fin_xtrain, Fin_ytrain, nb_epoch=500, batch_size=5, verbose=2)
-pred_goal_beh = submodel.predict(Fin_xtest)
-print "MODEL PREDICTS FOR GOALD DES ", pred_goal_beh
-print "Tru output is ", Fin_ytest
-#print "Average of tru output = ", sum(Y_train)/len(Y_train)
-
-# print "Weights = ", submodel.get_weights()
-# submodel.save_weights("submod.h5")
+sea.set(style="ticks", color_codes=True)
+g = sea.JointGrid(predicted.reshape(-1),norm_test.reshape(-1)) #, xlim=(-3,3), ylim=(-3,3))
+g = g.plot_joint(plt.scatter, color='#9b59b6', edgecolor="white", alpha='0.1')
+sea.plt.show()
+#g = g.plot_marginals(sea.distplot, kde=False, color="#9b59b6")
 
 ################
 #   Genes
